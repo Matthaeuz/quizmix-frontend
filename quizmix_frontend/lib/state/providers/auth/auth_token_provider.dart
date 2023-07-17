b8bc147f82d7c72671ef59b98cc5f692dc52bb96
Ron Matthew Alunan <ron.alunan@usjr.edu.ph>
2023-07-17 13:33:19 +0800

    Removed unecessary comments

--- quizmix_frontend/lib/state/providers/auth/auth_token_provider.dart
+++ quizmix_frontend/lib/state/providers/auth/auth_token_provider.dart
@@ -1,22 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quizmix_frontend/state/models/auth/auth_token.dart';
 
-// class AuthDetailsNotifier extends StateNotifier<User> {
-//   AuthDetailsNotifier(User user) : super(user);
-
-//   void updateUser(User newUser) {
-//     state = User(
-//       displayName: newUser.displayName,
-//       userId: newUser.userId,
-//       email: newUser.email,
-//     );
-//   }
-// }
-
-// final authDetailsProvider = StateNotifierProvider<AuthDetailsNotifier, User>((ref) {
-//   return AuthDetailsNotifier(User.base());
-// });
-
 class AuthTokenNotifier extends StateNotifier<AuthToken> {
   AuthTokenNotifier(AuthToken token) : super(token);
 
