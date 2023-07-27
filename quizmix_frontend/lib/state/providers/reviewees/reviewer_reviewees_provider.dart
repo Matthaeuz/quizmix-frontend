78a99eef9f240375ddaa7f694d4e924492acb8a5
wonn0 <113926116+wonn0@users.noreply.github.com>
2023-07-27 17:03:55 +0800

    Added backlog of to-be-refactored providers

--- quizmix_frontend/lib/state/providers/reviewees/reviewer_reviewees_provider.dart
+++ quizmix_frontend/lib/state/providers/reviewees/reviewer_reviewees_provider.dart
@@ -4,6 +4,7 @@ import 'package:quizmix_frontend/state/providers/api/rest_client_provider.dart';
 import 'package:quizmix_frontend/state/providers/auth/auth_token_provider.dart';
 import 'package:quizmix_frontend/state/providers/reviewers/reviewer_details_provider.dart';
 
+//Refactor later!
 final reviewerRevieweesProvider = FutureProvider.autoDispose<List<Reviewee>>((ref) async {
   final client = ref.watch(restClientProvider);
   final token = ref.watch(authTokenProvider);
--- quizmix_frontend/lib/state/providers/reviewees/unassigned_reviewees_provider.dart
+++ quizmix_frontend/lib/state/providers/reviewees/unassigned_reviewees_provider.dart
@@ -3,6 +3,7 @@ import 'package:quizmix_frontend/state/models/reviewees/reviewee.dart';
 import 'package:quizmix_frontend/state/providers/api/rest_client_provider.dart';
 import 'package:quizmix_frontend/state/providers/auth/auth_token_provider.dart';
 
+//Refactor later!
 final unassignedRevieweesProvider = FutureProvider.autoDispose<List<Reviewee>>((ref) async {
   final client = ref.watch(restClientProvider);
   final token = ref.watch(authTokenProvider);
