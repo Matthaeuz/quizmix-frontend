90eba4d472cd4bd53569217ce7d56729e687a72f
wonn0 <113926116+wonn0@users.noreply.github.com>
2023-08-12 18:14:02 +0800

    implemented empty placeholder to question bank

--- quizmix_frontend/lib/views/widgets/reviewer_question_bank/question_bank_list.dart
+++ quizmix_frontend/lib/views/widgets/reviewer_question_bank/question_bank_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quizmix_frontend/views/widgets/empty_data_placeholder.dart'; // Import the widget
 import 'package:quizmix_frontend/state/providers/questions/question_bank_provider.dart';
 import 'package:quizmix_frontend/views/widgets/reviewer_question_bank/question_bank_card.dart';
 
@@ -11,8 +12,16 @@ class QuestionBankList extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final questions = ref.watch(questionBankProvider);
+
     return questions.when(
       data: (questions) {
+        if (questions.isEmpty) {
+          return const EmptyDataPlaceholder(
+            message: "There are currently no questions in the bank.",
+            iconSize: 150,
+            fontSize: 18,
+          );
+        }
         return ListView.builder(
           shrinkWrap: true,
           physics: const NeverScrollableScrollPhysics(),
