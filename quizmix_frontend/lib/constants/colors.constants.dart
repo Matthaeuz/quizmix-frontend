e19b5e4cc91cbe7089037af37179faefe1e55962
aaronalcuitas <aaronalcuitas16@gmail.com>
2023-09-02 17:20:12 +0800

    added logo to login

--- quizmix_frontend/lib/constants/colors.constants.dart
+++ quizmix_frontend/lib/constants/colors.constants.dart
@@ -39,7 +39,7 @@ class AppColors {
   static const Color secondaryColor = Color(0xFF6F61C0);
   static const Color thirdColor = Color(0xFFA084E8);
   static const Color fourthColor = Color(0xFF8BE8E5);
-  static const Color fifthColor = Color(0xFFD5FFE4);
+  static const Color fifthColor = Color(0xFF9C54D5);
   static const Color lightBackgroundColor = Color(0xFFF5F5F5);
   static const Color black = Color(0xFF333333);
   static const Color grey = Color(0xFF9E9E9E);
--- quizmix_frontend/lib/views/screens/login_screen.dart
+++ quizmix_frontend/lib/views/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quizmix_frontend/constants/colors.constants.dart';
 import 'package:quizmix_frontend/views/widgets/sign_up_login/auth_background.dart';
 import 'package:quizmix_frontend/views/widgets/sign_up_login/login_fields.dart';
 
@@ -33,7 +34,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    // print('$screenWidth $screenHeight');
+    double logoContainerWidth = screenWidth * 0.3;
 
     // return Scaffold(
     //   appBar: null,
@@ -83,19 +84,90 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
       body: SingleChildScrollView(
         child: Container(
           width: screenWidth,
+          color: AppColors.secondaryColor,
           padding: EdgeInsets.fromLTRB(
               0, screenHeight * 0.05, 0, screenHeight * 0.05),
           child: Wrap(
             spacing: 24,
-            runSpacing: 24,
+            runSpacing: 8,
             alignment: WrapAlignment.center,
             crossAxisAlignment: WrapCrossAlignment.center,
             children: [
               Container(
-                width: screenWidth * 0.3,
-                height: screenWidth * 0.3,
-                color: Colors.purple,
+                width: logoContainerWidth,
+                height: logoContainerWidth,
                 constraints: logoBoxConstraints,
+                // decoration: BoxDecoration(
+                //   border: Border.all(
+                //     color: Colors.black,
+                //     width: 1.0,
+                //   ),
+                // ),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    double parentWidth = constraints.maxWidth;
+
+                    return Column(
+                      children: [
+                        Container(
+                          width: parentWidth * 0.7,
+                          height: parentWidth * 0.7,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                blurRadius: 16,
+                                offset: const Offset(2, 2),
+                              ),
+                            ],
+                          ),
+                          child: const Image(
+                            image: AssetImage(
+                                'lib/assets/images/QuizMix_Logo.png'),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        SizedBox(height: parentWidth * 0.05),
+                        RichText(
+                          text: TextSpan(
+                            style: TextStyle(
+                              fontSize: screenWidth < 1080 ? 32.0 : 48.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            children: const <TextSpan>[
+                              TextSpan(
+                                text: 'QuizMix',
+                                style: TextStyle(
+                                  color: AppColors.white,
+                                  shadows: <Shadow>[
+                                    Shadow(
+                                      offset: Offset(2.0, 2.0),
+                                      blurRadius: 8.0,
+                                      color: Colors.black,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              TextSpan(
+                                text: ' Code',
+                                style: TextStyle(
+                                  color: AppColors.fifthColor,
+                                  shadows: <Shadow>[
+                                    Shadow(
+                                      offset: Offset(2.0, 2.0),
+                                      blurRadius: 8.0,
+                                      color: Colors.black,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                ),
               ),
               Container(
                 width: screenWidth * 0.5,
