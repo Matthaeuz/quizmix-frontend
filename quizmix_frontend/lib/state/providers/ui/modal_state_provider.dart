6032a8f1ea689147744ac45f7866a11e59155183
aaronalcuitas <aaronalcuitas16@gmail.com>
2023-09-03 20:17:24 +0800

    refactored forgot password modal implementation

---
+++ quizmix_frontend/lib/state/providers/ui/modal_state_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum ModalState { none, forgotPassword }
+
+class ModalStateNotifier extends StateNotifier<ModalState> {
+  ModalStateNotifier(ModalState initModalState) : super(initModalState);
+
+  void updateModalState(ModalState newModalState) {
+    state = newModalState;
+  }
+}
+
+final modalStateProvider =
+    StateNotifierProvider<ModalStateNotifier, ModalState>((ref) {
+  return ModalStateNotifier(ModalState.none);
+});
