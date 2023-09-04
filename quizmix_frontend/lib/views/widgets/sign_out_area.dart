fb5dbcfa46ebd35e9bf9b97762cb81ede4980afd
aaronalcuitas <aaronalcuitas16@gmail.com>
2023-09-05 00:44:43 +0800

    completed sign up

--- quizmix_frontend/lib/views/widgets/sign_out_area.dart
+++ quizmix_frontend/lib/views/widgets/sign_out_area.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:quizmix_frontend/state/models/auth/auth_token.dart';
+import 'package:quizmix_frontend/state/models/users/user.dart';
+import 'package:quizmix_frontend/state/providers/auth/auth_token_provider.dart';
+import 'package:quizmix_frontend/state/providers/users/user_details_provider.dart';
+import 'package:quizmix_frontend/views/screens/login_screen.dart';
 
-class SignOutArea extends StatelessWidget {
+class SignOutArea extends ConsumerWidget {
   const SignOutArea({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return LayoutBuilder(
       builder: ((context, constraints) {
         final parentWidth = constraints.maxWidth;
 
         return InkWell(
-          onTap: () {},
+          onTap: () {
+            ref.read(userProvider.notifier).updateUser(User.base());
+            ref.read(authTokenProvider.notifier).updateToken(AuthToken.base());
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()));
+          },
           child: Padding(
             padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
             child: Row(
