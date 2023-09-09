9407d7e34178bf7f1c9c0144fdd6f7ac121d8783
aaronalcuitas <aaronalcuitas16@gmail.com>
2023-09-10 02:35:49 +0800

    removed dead code

--- quizmix_frontend/lib/views/widgets/reviewee_home/reviewee_question_bank_tab.dart
+++ quizmix_frontend/lib/views/widgets/reviewee_home/reviewee_question_bank_tab.dart
@@ -5,9 +5,6 @@ import 'package:quizmix_frontend/state/providers/questions/question_bank_provide
 import 'package:quizmix_frontend/state/providers/ui/modal_state_provider.dart';
 import 'package:quizmix_frontend/views/widgets/empty_data_placeholder.dart';
 import 'package:quizmix_frontend/views/widgets/question_bank/question_bank_item.dart';
-import 'package:quizmix_frontend/views/widgets/reviewer_question_bank/question_bank_list.dart';
-import 'package:quizmix_frontend/views/widgets/reviewer_question_bank/question_bank_question_view.dart';
-import 'package:quizmix_frontend/views/widgets/solid_button.dart';
 
 class RevieweeQuestionBankTab extends ConsumerStatefulWidget {
   const RevieweeQuestionBankTab({
@@ -232,50 +229,6 @@ class _RevieweeQuestionBankTabState
           ),
         ),
       ),
-    );
-    return Row(
-      children: [
-        // Middle Section
-        Expanded(
-          flex: 3,
-          child: Container(
-            color: AppColors.fifthColor,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(25),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        SolidButton(
-                          text: 'Search',
-                          onPressed: () {},
-                          icon: const Icon(Icons.search),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const Expanded(
-                  child: SingleChildScrollView(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 25),
-                      child: Column(
-                        children: [QuestionBankList()],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-
-        // Right Section
-        const QuestionBankQuestion()
-      ],
     );
   }
 }
