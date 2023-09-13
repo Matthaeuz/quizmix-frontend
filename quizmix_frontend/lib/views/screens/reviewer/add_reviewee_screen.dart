43e882c5a0436d77ef7757418f6bb9ae29ba28d2
Aloysius Beronque <aloysiusmatthew1@gmail.com>
2023-09-14 00:15:22 +0800

    Search Reviewee from add reviewee screen

--- quizmix_frontend/lib/views/screens/reviewer/add_reviewee_screen.dart
+++ quizmix_frontend/lib/views/screens/reviewer/add_reviewee_screen.dart
@@ -10,9 +10,20 @@ import 'package:quizmix_frontend/views/widgets/empty_data_placeholder.dart';
 import 'package:quizmix_frontend/views/widgets/reviewer_reviewee_list/AddRevieweeCard.dart';
 import 'package:quizmix_frontend/views/widgets/search_input.dart';
 import 'package:quizmix_frontend/views/widgets/solid_button.dart';
-// import 'package:quizmix_frontend/state/providers/api/rest_client_provider.dart';
-// import 'package:quizmix_frontend/state/providers/auth/auth_token_provider.dart';
 import 'package:quizmix_frontend/state/providers/reviewees/unassigned_reviewees_provider.dart'; // Import the provider
+
+class SearchTermNotifier extends StateNotifier<String> {
+  SearchTermNotifier() : super('');
+
+  void setSearchTerm(String term) {
+    state = term;
+  }
+}
+
+final searchTermProvider =
+    StateNotifierProvider<SearchTermNotifier, String>((ref) {
+  return SearchTermNotifier();
+});
 
 class AddRevieweeScreen extends ConsumerStatefulWidget {
   const AddRevieweeScreen({Key? key}) : super(key: key);
@@ -24,6 +35,7 @@ class AddRevieweeScreen extends ConsumerStatefulWidget {
 class _AddRevieweeScreenState extends ConsumerState<AddRevieweeScreen> {
   List<bool> isCheckedList = [];
   List<User> selectedReviewees = [];
+  Map<int, bool> isCheckedMap = {}; // Use a Map to store selected state
 
   void printSelectedReviewees() {
     for (var reviewee in selectedReviewees) {
@@ -40,13 +52,17 @@ class _AddRevieweeScreenState extends ConsumerState<AddRevieweeScreen> {
     selectedReviewees.clear();
     await reviewees.when(
       data: (reviewees) async {
-        for (int i = 0; i < isCheckedList.length; i++) {
-          if (isCheckedList[i]) {
-            final reviewee = reviewees[i];
+        for (int i = 0; i < reviewees.length; i++) {
+          final reviewee = reviewees[i];
+          final revieweeId = reviewee.id;
+
+          // Check if the reviewee with this ID is selected in isCheckedMap
+          if (isCheckedMap.containsKey(revieweeId) &&
+              isCheckedMap[revieweeId] == true) {
             selectedReviewees.add(reviewee);
 
             final details = AssignRevieweeDetails(
-              revieweeId: reviewee.id,
+              revieweeId: revieweeId,
               reviewerId: reviewer.id,
             );
             await client.assignReviewee(token, details);
@@ -72,6 +88,26 @@ class _AddRevieweeScreenState extends ConsumerState<AddRevieweeScreen> {
   @override
   Widget build(BuildContext context) {
     final revieweesFuture = ref.watch(unassignedRevieweesProvider);
+
+    // Step 1: Read the current search term from the state
+    final searchTerm = ref.watch(searchTermProvider);
+
+    // Step 2: Filter the reviewees based on the search term
+    final filteredReviewees = revieweesFuture.when(
+      data: (reviewees) {
+        return reviewees.where((reviewee) {
+          final fullName = reviewee.fullName.toLowerCase();
+          return fullName.contains(searchTerm.toLowerCase());
+        }).toList();
+      },
+      loading: () => [], // Return an empty list during loading
+      error: (_, __) => [], // Return an empty list on error
+    );
+
+    // Initialize isCheckedList with the same number of elements as filteredReviewees
+    if (isCheckedList.length != filteredReviewees.length) {
+      isCheckedList = List.generate(filteredReviewees.length, (index) => false);
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -105,7 +141,10 @@ class _AddRevieweeScreenState extends ConsumerState<AddRevieweeScreen> {
                     alignment: Alignment.centerLeft,
                     child: SearchInput(
                       onChanged: (value) {
-                        // Handle search input changes
+                        // Step 3: Update the search term in the state
+                        ref
+                            .read(searchTermProvider.notifier)
+                            .setSearchTerm(value);
                       },
                     ),
                   ),
@@ -121,41 +160,30 @@ class _AddRevieweeScreenState extends ConsumerState<AddRevieweeScreen> {
             ),
             const SizedBox(height: 20),
             Expanded(
-              child: revieweesFuture.when(
-                data: (reviewees) {
-                  if (reviewees.isEmpty) {
-                    return const EmptyDataPlaceholder(
+              child: filteredReviewees.isEmpty
+                  ? const EmptyDataPlaceholder(
                       message: "No unassigned reviewees found.",
-                    );
-                  }
+                    )
+                  : ListView.separated(
+                      itemCount: filteredReviewees.length,
+                      separatorBuilder: (context, index) =>
+                          const SizedBox(height: 25),
+                      itemBuilder: (context, index) {
+                        final reviewee = filteredReviewees[index];
 
-                  if (isCheckedList.isEmpty) {
-                    // Initialize isCheckedList if it hasn't been initialized yet
-                    isCheckedList = List.filled(reviewees.length, false);
-                  }
-
-                  return ListView.separated(
-                    itemCount: reviewees.length,
-                    separatorBuilder: (context, index) =>
-                        const SizedBox(height: 25),
-                    itemBuilder: (context, index) {
-                      final reviewee = reviewees[index];
-
-                      return AddRevieweeCard(
-                        reviewee: reviewee,
-                        isSelected: isCheckedList[index],
-                        onCheckboxChanged: (value) {
-                          setState(() {
-                            isCheckedList[index] = value;
-                          });
-                        },
-                      );
-                    },
-                  );
-                },
-                loading: () => const CircularProgressIndicator(),
-                error: (error, stackTrace) => const Text('An error occurred'),
-              ),
+                        return AddRevieweeCard(
+                          reviewee: reviewee,
+                          isSelected: isCheckedMap.containsKey(reviewee.id) &&
+                              isCheckedMap[reviewee.id] == true,
+                          onCheckboxChanged: (value) {
+                            setState(() {
+                              isCheckedMap[reviewee.id] =
+                                  value; // Update the Map with the selected state
+                            });
+                          },
+                        );
+                      },
+                    ),
             ),
           ],
         ),
