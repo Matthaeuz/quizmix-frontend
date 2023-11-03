85c9510db1608366cb5606a0c02f119b1b800417
wonn0 <113926116+wonn0@users.noreply.github.com>
2023-11-03 19:16:08 +0800

    made grid scrollable

--- quizmix_frontend/lib/views/widgets/question_grid.dart
+++ quizmix_frontend/lib/views/widgets/question_grid.dart
@@ -12,31 +12,32 @@ class QuestionGrid extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final questions = ref.watch(currentQuizViewedProvider).questions;
-    // final screenWidth = MediaQuery.of(context).size.width;
 
+    // try intrinsicheight also :))
     return Expanded(
-        child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-                decoration: BoxDecoration(
-                  color: AppColors.white,
-                  border: Border.all(
-                    color: AppColors.mainColor,
-                    width: 2.0,
-                  ),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(12.0),
-                    child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Wrap(spacing: 8, runSpacing: 8, children: [
-                          for (int i = 0; i < questions.length; i++) ...[
-                            SolidGridItem(
-                              label: "${i + 1}",
-                              onPressed: onPressed,
-                            )
-                          ]
-                        ]))))));
+        child: SingleChildScrollView(
+            child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.white,
+                      border: Border.all(
+                        color: AppColors.mainColor,
+                        width: 2.0,
+                      ),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12.0),
+                        child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Wrap(spacing: 8, runSpacing: 8, children: [
+                              for (int i = 0; i < questions.length; i++) ...[
+                                SolidGridItem(
+                                  label: "${i + 1}",
+                                  onPressed: onPressed,
+                                )
+                              ]
+                            ])))))));
   }
 }
